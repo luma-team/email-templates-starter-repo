2a08b337968204ede5987c02954418f72d2c6e49
Victor Pontis <victor@pont.is>
2021-10-11 01:35:05 -0400

    Add segment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,35 @@
-import "tailwindcss/tailwind.css";
-import "../styles/globals.css";
-import toast, { Toaster } from "react-hot-toast";
+import * as snippet from "@segment/snippet";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 import ReactDOM from "react-dom";
 
-import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import "tailwindcss/tailwind.css";
 import { useIsMounted } from "../components/hooks/useIsMounted";
+import "../styles/globals.css";
+
+function renderSnippet() {
+  const opts = {
+    apiKey: "2PqnHBdT8XbhzAddB9rmBRNAvmZciLo7",
+    page: true,
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    return snippet.max(opts);
+  }
+
+  return snippet.min(opts);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted();
   return (
     <>
+      <Script
+        id="segment"
+        dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+      />
+
       <Component {...pageProps} />
       {isMounted &&
         ReactDOM.createPortal(
